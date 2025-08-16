7bd99dce60f3ef559b9622de5f19554fb8982e6b
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2025-08-16 18:09:37 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
-x=int(input("ENTER 1 TO CONTINUE ELSE ENTER ANY OTHER NUMBER:-"))
-while x=1:
+x=1
+while x==1:
     a=float(input("Enter 1st number"))
     b=float(input("Enter 2nd number"))
     print("Sum =",a+b)
     print("Product =",a*b)
     print("Division =",a/b)
     print("Subtract =",a-b)
+    x=int(input("ENTER 1 TO CONTINUE ELSE ENTER ANY OTHER NUMBER:-"))
 
     
+
