99c019524c41311a96ccd3e1eed65d6965022c3e
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2025-08-16 17:48:50 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+x=int(input("ENTER 1 TO CONTINUE ELSE ENTER ANY OTHER NUMBER:-"))
+while x=1:
+    a=float(input("Enter 1st number"))
+    b=float(input("Enter 2nd number"))
+    print("Sum =",a+b)
+    print("Product =",a*b)
+    print("Division =",a/b)
+    print("Subtract =",a-b)
+
+    
